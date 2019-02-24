597082f649b680473d0a2daca76b0ce64de5d632
lEmanl <36420383+lEmanl@users.noreply.github.com>
2019-02-23 23:18:40 -0600

    Add files via upload

---
+++ web_scrap.js
@@ -0,0 +1,51 @@
+const https = require("https")
+let cheerio = require("cheerio")
+let jsonframe = require("jsonframe-cheerio")
+
+/*format that we want our data in
+
+event-detail 
+
+date (category-name)
+|
+|_title         (div cat-events -> span featured-title)
+|_day           (span day)
+|_month         (span month)
+|_day number    (span day-left)
+|_time          (span event-time-left id tleft)
+|_location      (span featured-location span orange)
+
+*/
+
+let events = []
+
+/*
+    Gets our html data from utdallas calendar website
+*/
+https.get("https://www.utdallas.edu/calendar/mobile/events-by-date.php?type=thirty", (res) => {
+    let data = '';
+   
+    res.on('data', (chunk) => {
+        data += chunk
+    })
+
+    res.on('end', () => {
+        console.log("Finished collecting data!")
+        let $ = cheerio.load(data)
+
+        $('#event-detail .category-name').each(function(index, element){
+            console.log($(element).text())
+            events[index] = {}
+            events[index]['name'] = $(element).text()
+        })
+        
+        /*
+        jsonframe($)
+        let weekly_data = $("body #event-detail div").scrape(frame)
+
+        console.log("parsed data: " + weekly_data)
+        */
+    })
+}).on("error", (err) => {
+    console.log("Error: " + err.message)
+})
